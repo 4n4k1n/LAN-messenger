82c362628053e892a834da3188ec61c27a9edebc
4n4k1n <anakin@pregitzer.de>
2025-07-04 13:51:49 +0200

    updated install script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
 git clone https://github.com/4n4k1n/local-network-messenger.git ~/local-network-messenger
 
-read -p "Enter IP address: " ip_address
+if [ $# -eq 0 ]; then
+    read -p "Enter IP address: " ip_address
+else
+    ip_address=$1
+fi
 
 make -C ~/local-network-messenger
-
 ~/local-network-messenger/client $ip_address
-
-rm -rf ~/local-network-messenger 
+rm -rf ~/local-network-messenger
